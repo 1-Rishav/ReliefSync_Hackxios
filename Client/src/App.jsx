c7d8e95af63b065e8d97013491e865029b767ded
Rishav <rajrishav011@gmail.com>
2025-12-24 22:49:46 +0530

    Initialize React application with Vite and styling

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import Router from './routes/index'
+
+const App = () => {
+  
+  return (
+    <>
+      <Router />
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/main.jsx
@@ -0,0 +1,48 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { HeroUIProvider } from "@heroui/react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { store, persistor } from './store/index.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { WalletProvider } from './components/Common_Features/WalletProvider.jsx'
+import { setupOneSignal } from './OneSignalSetup/OneSignalProvider.jsx'
+import './index.css'
+import App from './App.jsx'
+
+async function initializeApp() {
+  //await setupOneSignal();
+  createRoot(document.getElementById('root')).render(
+
+    <StrictMode>
+
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <HeroUIProvider>
+              <WalletProvider>
+                <App />
+              </WalletProvider>
+              <ToastContainer
+                position="bottom-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                theme="dark"
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              />
+            </HeroUIProvider>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+
+    </StrictMode>
+  )
+}
+initializeApp();
