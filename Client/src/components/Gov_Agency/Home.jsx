d97ee028dcb3d8eb22a96a1e4214104fd9b6072a
Rishav <rajrishav011@gmail.com>
2025-12-25 14:50:12 +0530

    Add Home component for the Gov Agency section

---
+++ Client/src/components/Gov_Agency/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HomeSetup from '../Common_Features/HomeSetup'
+import TodayTask from '../NGO/TodayTask'
+import FeedBackBackground from '../Common_Features/FeedBack'
+import { Footer } from '../Common_Features/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <HomeSetup/>
+    <TodayTask/>
+    <FeedBackBackground/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
