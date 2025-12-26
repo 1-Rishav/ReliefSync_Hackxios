53d361148eb58922cf850d1415c25f78a94741cf
Rishav <rajrishav011@gmail.com>
2025-12-26 10:08:12 +0530

    Add NGOHomePage component to render the NGO Home interface

---
+++ Client/src/pages/NGOPages/NGOHomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../../components/NGO/Home'
+
+const NGOHomePage = () => {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default NGOHomePage
