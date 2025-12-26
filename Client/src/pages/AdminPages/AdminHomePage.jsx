db5a98902a15f89e0992fe1313805e42769f9562
Rishav <rajrishav011@gmail.com>
2025-12-26 10:06:04 +0530

    Add AdminHomePage component to render the Home component for admin users

---
+++ Client/src/pages/AdminPages/AdminHomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../../components/Admin/Home'
+
+const AdminHomePage = () => {
+  return (
+    <>
+    <Home/>
+    </>
+    
+  )
+}
+
+export default AdminHomePage
