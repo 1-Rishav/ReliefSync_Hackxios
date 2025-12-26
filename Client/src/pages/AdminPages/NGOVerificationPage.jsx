fd0bfac6521933b1d7fbb024d0917867c949075f
Rishav <rajrishav011@gmail.com>
2025-12-26 10:07:48 +0530

    Add NGOVerificationPage and AgentHomePage components for admin and agency interfaces

---
+++ Client/src/pages/AdminPages/NGOVerificationPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NGOVerification from '../../components/Admin/NGOVerification'
+
+const NGOVerificationPage = () => {
+  return (
+    <>
+    <NGOVerification/>
+    </>
+  )
+}
+
+export default NGOVerificationPage
---
+++ Client/src/pages/Gov_AgencyPages/AgentHomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../../components/Gov_Agency/Home'
+
+const AgentHomePage = () => {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default AgentHomePage
