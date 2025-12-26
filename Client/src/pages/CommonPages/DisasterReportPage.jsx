8ed2e885bc17214b6835f00caac77564e00bd2c3
Rishav <rajrishav011@gmail.com>
2025-12-26 10:07:31 +0530

    Add DisasterReportPage component to display disaster report features

---
+++ Client/src/pages/CommonPages/DisasterReportPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DisasterReport from '../../components/Common_Features/DisasterReport'
+
+const DisasterReportPage = () => {
+  return (
+    <>
+    <DisasterReport/>
+    </>
+  )
+}
+
+export default DisasterReportPage
