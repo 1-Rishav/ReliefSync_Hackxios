1ec2d286a29cf229ad7f3931ef7de46858bf2915
Rishav <rajrishav011@gmail.com>
2025-12-26 10:06:44 +0530

    Add HelpDeskPage component to integrate HelpDesk functionality for citizens

---
+++ Client/src/pages/CitizenPages/HelpDeskPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HelpDesk from '../../components/Citizen/HelpDesk'
+
+const HelpDeskPage = () => {
+  return (
+    <>
+    <HelpDesk/>
+    </>
+  )
+}
+
+export default HelpDeskPage
