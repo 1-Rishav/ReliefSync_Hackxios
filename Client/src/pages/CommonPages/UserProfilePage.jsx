d91f8e7ea2d193d705edd1121f653b0ad44bb3cd
Rishav <rajrishav011@gmail.com>
2025-12-27 11:27:28 +0530

    Add UserProfilePage component to render user profile features

---
+++ Client/src/pages/CommonPages/UserProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserProfile from '../../components/Common_Features/UserProfile'
+
+const UserProfilePage = () => {
+  return (
+    <UserProfile/>
+  )
+}
+
+export default UserProfilePage
