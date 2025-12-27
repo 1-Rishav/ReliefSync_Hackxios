597d585eb4bd8a294e8fb0535e9c2ae8e006569d
Rishav <rajrishav011@gmail.com>
2025-12-27 11:27:11 +0530

    Add AllocatedTaskPage component to display allocated task features

---
+++ Client/src/pages/CommonPages/AllocatedTaskPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AllocatedTask from '../../components/Common_Features/AllocatedTask'
+
+const AllocatedTaskPage = () => {
+  return (
+    <AllocatedTask/>
+  )
+}
+
+export default AllocatedTaskPage
