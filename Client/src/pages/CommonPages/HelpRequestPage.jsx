1e1b502c96dcdaf8abd522fc29c1ff61ae8a715e
Rishav <rajrishav011@gmail.com>
2025-12-27 11:27:22 +0530

    Add HelpRequestPage component to render help request features

---
+++ Client/src/pages/CommonPages/HelpRequestPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HelpRequest from '../../components/Common_Features/HelpRequest.jsx'
+
+const HelpRequestPage = () => {
+  return (
+    <HelpRequest/>
+  )
+}
+
+export default HelpRequestPage
