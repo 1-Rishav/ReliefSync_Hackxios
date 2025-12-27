3255220c2123207d97307fcf3c9106eca37a9a9c
Rishav <rajrishav011@gmail.com>
2025-12-27 11:27:33 +0530

    Add EventTablePage component to display event table features

---
+++ Client/src/pages/CommonPages/EventTablePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventTable from '../../components/Common_Features/EventTable'
+
+const EventTablePage = () => {
+  return (
+    <>
+    <EventTable/>
+    </>
+  )
+}
+
+export default EventTablePage
