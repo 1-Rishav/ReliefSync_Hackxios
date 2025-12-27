88a181002b1aba9be1091264f058cfd346b785fb
Rishav <rajrishav011@gmail.com>
2025-12-27 11:27:39 +0530

    Add DeliveredPage component to render delivered items features

---
+++ Client/src/pages/CommonPages/DeliveredPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Delivered from '../../components/Common_Features/DeliveredTable'
+
+const DeliveredPage = () => {
+  return (
+    <Delivered/>
+  )
+}
+
+export default DeliveredPage
