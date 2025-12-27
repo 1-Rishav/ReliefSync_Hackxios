9df725615a0d8dbce59acd7298799db844f99d6c
Rishav <rajrishav011@gmail.com>
2025-12-27 23:18:42 +0530

    Add ActiveAllocationPage component to display active allocations

---
+++ Client/src/pages/AdminPages/ActiveAllocationPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ActiveAllocation from '../../components/Admin/ActiveAllocation'
+
+const ActiveAllocationPage = () => {
+  return (
+    <>
+    <ActiveAllocation/>
+    </>
+  )
+}
+
+export default ActiveAllocationPage
