e63014e81ffc8d6a9a3db427cd32bf8ab594ddd2
Rishav <rajrishav011@gmail.com>
2025-12-27 11:27:17 +0530

    Add UrgentVoiceMailPage component to display urgent voice mail features

---
+++ Client/src/pages/CommonPages/UrgentVoiceMailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UrgentVoiceMail from '../../components/Common_Features/UrgentVoiceMail'
+
+const UrgentVoiceMailPage = () => {
+  return (
+    <>
+    <UrgentVoiceMail/>
+    </>
+  )
+}
+
+export default UrgentVoiceMailPage
